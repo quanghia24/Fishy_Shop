5d5e800e2649459173bdf6936e2c1da674baff48
quanghia24 <nghiaqcqs@gmail.com>
2024-10-26 09:10:47 +0700

    import products

--- backend/base/views/product_views.py
+++ backend/base/views/product_views.py
@@ -15,7 +15,7 @@ def getProducts(request):
         query = ''
     products = Product.objects.filter(name__icontains=query).order_by('_id')
     page = request.query_params.get('page')
-    paginator = Paginator(products, 4)
+    paginator = Paginator(products, 12)
 
     try:
         products = paginator.page(page)
